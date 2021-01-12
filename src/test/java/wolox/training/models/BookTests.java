f107aad6c5ecae796c86e8833174c7ddaff4f016
Juan Pablo <juanpbltbn@gmail.com>
2021-01-12 14:33:45 -0500

    Test for model constraints done

---
+++ src/test/java/wolox/training/models/BookTests.java
@@ -0,0 +1,59 @@
+package wolox.training.models;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+import wolox.training.repositories.BookRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class BookTests {
+
+	@Autowired
+	private TestEntityManager entityManager;
+	@Autowired
+	private BookRepository bookRepository;
+
+	private Book book;
+
+	@BeforeEach
+	public void init() {
+
+		this.book = new Book();
+		this.book.setImage("http://someurl.com");
+		this.book.setGenre("Book genre");
+		this.book.setPages(500);
+		this.book.setPublisher("Some publisher");
+		this.book.setTitle("Book title");
+		this.book.setSubtitle("Book subtitle");
+		this.book.setYear("2000");
+		this.book.setIsbn("Some isbn");
+		this.book.setAuthor("Book author");
+	
+	}
+
+	@Test
+	void givenBook_whenCreate_thenSuccess() {
+		entityManager.persist(book);
+		entityManager.flush();
+
+		Optional<Book> newBook = bookRepository.findTopByAuthor("Book author");
+
+		assertThat(newBook).isNotEmpty();
+		assertThat(newBook.get().getTitle()).isEqualTo(book.getTitle());
+	}
+
+	@Test
+	void givenBook_whenCreate_thenPreconditionsException() {
+		assertThrows(NullPointerException.class, () -> book.setTitle(null));
+	}
+
+}
---
+++ src/test/java/wolox/training/models/UserTests.java
@@ -0,0 +1,54 @@
+package wolox.training.models;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+import wolox.training.repositories.UserRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UserTests {
+
+	@Autowired
+	private TestEntityManager entityManager;
+	@Autowired
+	private UserRepository userRepository;
+
+	private User user;
+
+	@BeforeEach
+	public void init() {
+
+		this.user = new User();
+		this.user.setName("TestName");
+		this.user.setUsername("testUsername");
+		this.user.setBirthdate(LocalDate.now());
+
+	}
+
+	@Test
+	void givenUser_whenCreate_thenSuccess() {
+		entityManager.persist(user);
+		entityManager.flush();
+
+		Optional<User> newUser = userRepository.findByUsername("testUsername");
+
+		assertThat(newUser).isNotEmpty();
+		assertThat(newUser.get().getUsername()).isEqualTo(user.getUsername());
+	}
+
+	@Test
+	void givenUser_whenCreate_thenPreconditionsException() {
+		assertThrows(NullPointerException.class, () -> user.setUsername(null));
+	}
+
+}
